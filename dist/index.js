fe8abc6ae63a5482aabbbd755db0af2480655423
Ashish Kurmi <akurmi@stepsecurity.io>
2023-11-30 15:09:05 -0800

    adding subscription check

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = require("axios");
 const core = require("@actions/core");
 const helpers_1 = require("./helpers");
 const processor_1 = require("./processor");
 const processor = new processor_1.Processor();
+async function validateSubscription() {
+    const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`;
+    try {
+        await axios_1.default.get(API_URL, { timeout: 3000 });
+    }
+    catch (error) {
+        if ((0, axios_1.isAxiosError)(error) && error.response) {
+            core.error('Subscription is not valid. Reach out to support@stepsecurity.io');
+            process.exit(1);
+        }
+        else {
+            core.info('Timeout or API not reachable. Continuing to next step.');
+        }
+    }
+}
 (async () => {
     var _a;
+    await validateSubscription();
     const input = {
         // Common
         operation: (_a = core.getInput("operation")) === null || _a === void 0 ? void 0 : _a.toLowerCase(),
